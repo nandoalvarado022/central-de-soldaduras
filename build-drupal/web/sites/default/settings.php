c362b84fd96a7c11e9e4eea8a4010b8cf891b749
Nando Alvarado <nandoalvarado022@gmail.com>
2025-12-14 23:25:48 -0500

    adding web folder build-drupal

--- build-drupal/web/sites/default/settings.php
+++ build-drupal/web/sites/default/settings.php
@@ -893,11 +893,11 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
 $databases['default']['default'] = array (
-  'database' => 'cspi_web_test',
+  'database' => 'test_bd',
   'username' => 'root',
   'password' => 'test',
   'prefix' => '',
-  'host' => 'cspi_db',
+  'host' => 'db',
   'port' => '3306',
   'isolation_level' => 'READ COMMITTED',
   'driver' => 'mysql',
