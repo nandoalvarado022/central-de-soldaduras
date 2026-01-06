e9966e47b778bf740f1401ea5117e115ac1642df
Nando (Hernando Alvarado) <nandoalvarado022@gmail.com>
2026-01-06 17:30:20 -0500

    ajuste

--- build-drupal/web/sites/default/settings.php
+++ build-drupal/web/sites/default/settings.php
@@ -905,3 +905,4 @@ $databases['default']['default'] = array (
   'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
 );
 $settings['config_sync_directory'] = '../config/sync';
+$config['system.logging']['error_level'] = 'verbose'; // Ver errores desarrollador
